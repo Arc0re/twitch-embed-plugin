a93487b44b87bae56731d8574e3fb9ac974cc99c
Thomas Guyot <PaperboyCheero@yahoo.com>
2015-09-24 00:05:06 +0200

    Its now using full jquery, and can be duplicated.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Twitch jquery plugin
+
++ Loads the stream only when "showed"
++ Unloads it when "hidden"
+
+__This could be optimized.__
+
+Currently, you have to duplicate the plugin functions for each caster. This is far from being optimized !
--- toggle.js
+++ toggle.js
@@ -1,7 +1,5 @@
+/* This adds the player to the page. KEEP IN MIND THAT YOU NEED A DIFFERENT ID FOR EACH #video, iframe,... FOR THIS TO WORK. */
 function showVideo() {
-
-    /* This adds the player to the page. KEEP IN MIND THAT YOU NEED A DIFFERENT ID FOR EACH FOR THIS TO WORK. */
-
    $('#video').append('<iframe id="cast1" src="http://www.twitch.tv/uknighted/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe>');
 }
 
@@ -9,10 +7,8 @@ function hideVideo() {
     $('#cast1').remove(); // Dont forget to change cast1 for the id you chose in showVideo()
 }
 
-function showHide() {
-
-    /* This does the regular link switching stuff. Nothing to change */
-
+/* This does the regular link switching stuff. Nothing to change */
+$('#show-hide-link').click(function(){
     if ($('#show-hide-link').text() == 'Hide Video') {
         hideVideo();
         $('#show-hide-link').text('Show Video');
@@ -21,4 +17,26 @@ function showHide() {
         showVideo();
         $('#show-hide-link').text('Hide Video');
     }
+});
+
+/* Example, displaying another stream : just clome the above functions (same in HTML) */
+function showVideo2() {
+   $('#video2').append('<iframe id="cast2" src="http://www.twitch.tv/crunk_muffin/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe>');
 }
+
+function hideVideo2() {
+    $('#cast2').remove(); // Dont forget to change cast1 for the id you chose in showVideo()
+}
+
+$('#show-hide-link2').click(function(){
+    if ($('#show-hide-link2').text() == 'Hide Video') {
+        hideVideo2();
+        $('#show-hide-link2').text('Show Video');
+    }
+    else {
+        showVideo2();
+        $('#show-hide-link2').text('Hide Video');
+    }
+});
+
+// etc etc
